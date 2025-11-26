2e1dc9ef09b58a934c116414c76c35d41c999732
sagar <sagarkashyap5587@gmail.com>
2025-11-26 13:25:02 +0530

    feat(Portfolio): working on add loader in tab

--- app/page.hook.ts
+++ app/page.hook.ts
@@ -1,12 +1,26 @@
 // <================== Import Dependencies ==================>
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 // <================== Use Portfolio Hooks ==================>
 export const usePortfolio = () => {
   // <================== All States==================>
   const [activeTab, setActiveTab] = useState(0);
+
+  useEffect(() => {
+    const contentArea = document.querySelector(".content-area");
+    if (contentArea) {
+      contentArea.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }, [activeTab]);
 
   // <================== Retun Data ==================>
   return {
